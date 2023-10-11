621d0d63e379f25c4d5855ef9330cfafdf8017f6
sebastian-arias369 <145506818+sebastian-arias369@users.noreply.github.com>
2023-10-11 17:19:18 -0500

    Add files via upload

---
+++ salas_service1/src/main/java/com/example/salas_service1/SalasService1Application.java
@@ -0,0 +1,13 @@
+package com.example.salas_service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalasService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalasService1Application.class, args);
+    }
+
+}
---
+++ salas_service1/src/test/java/com/example/salas_service1/SalasService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salas_service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
